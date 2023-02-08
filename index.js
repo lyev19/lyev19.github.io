249dd67748fd4acc9f5ad284f26b04449be20334
leon <leonwanzo77@gmail.com>
2023-02-07 23:17:44 -0300

    fixed load

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 
 const profilePic = document.getElementById("profile-pic")
-
 const firstName = document.getElementById("first-name")
 const secondName = document.getElementById("second-name")
 const descritption = document.querySelector(".aboutme-description");
@@ -9,22 +8,29 @@ const number = document.getElementById("number")
 const location1 = document.getElementById("location")
 const specs = document.querySelectorAll(".information1")
 const cardContainer = document.querySelectorAll(".card-container")
+
+
+
+
+
 cardManager(cardContainer);
+let fetches = JSON.parse(localStorage.getItem("data"))
+document.onload= a()
 
-console.log(specs)
 
 
-function a(){
+async function a(){
+    console.log(localStorage.getItem("data"))
     if(localStorage.getItem("data")===null){
-        let data ={}
-         api_call().then(
-             data = JSON.parse(localStorage.getItem("data"))
-             
-         ).then(
-            console.log(data)
-            
-         )
-         
+
+       const res = await api_call()
+       fetches = JSON.parse(localStorage.getItem("data"))
+       firstName.innerHTML = fetches.name.first
+       secondName.innerHTML = fetches.name.last
+       profilePic.src = fetches.picture.large
+       email.innerHTML = fetches.email
+       number.innerHTML= fetches.cell
+       location1.innerHTML = fetches.location.street.name +" "+ fetches.location.street.number + " "+ fetches.location.city +", "+fetches.location.state
     
         
     }
@@ -32,14 +38,20 @@ function a(){
     else{
         const data = JSON.parse(localStorage.getItem("data"))
         console.log(data)
+
+         firstName.innerHTML = fetches.name.first
+         secondName.innerHTML = fetches.name.last
+         profilePic.src = fetches.picture.large
+         email.innerHTML = fetches.email
+         number.innerHTML= fetches.cell
+         location1.innerHTML = fetches.location.street.name +" "+ fetches.location.street.number + " "+ fetches.location.city +", "+fetches.location.state
     }
     
     }
     
 
 
-window.onload = a()
-window.onreset = a()
+
 
 async function api_call ( ){
     
@@ -52,14 +64,9 @@ async function api_call ( ){
 
 }
 
-const fetches = JSON.parse(localStorage.getItem("data"))
 
-firstName.innerHTML = fetches.name.first
-secondName.innerHTML = fetches.name.last
-profilePic.src = fetches.picture.large
-email.innerHTML = fetches.email
-number.innerHTML= fetches.cell
-location1.innerHTML = fetches.location.street.name +" "+ fetches.location.street.number + " "+ fetches.location.city +", "+fetches.location.state
+
+
 
 
 function cardManager (cc){
